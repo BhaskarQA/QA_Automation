6f6916955d5b775f438f565d9b2adb4c044b5324
Bhaskar Srinivasan <bhaskar.srinivasan@talentship.io>
2023-07-25 17:54:57 +0530

    Ui to backend api test script

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,13 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress'); 
 
-module.exports = defineConfig({
+module.exports = defineConfig({ 
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       on('file:preprocessor', cucumber())
+      
     },
     specPattern: 'cypress/integration/cucumber-tests/*.feature',
 

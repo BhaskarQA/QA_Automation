e23ae7a65ee459d6999753003104480bd27d446c
Bhaskar Srinivasan <bhaskar.srinivasan@talentship.io>
2023-07-26 11:44:23 +0530

    config error resolved

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,12 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
-const { defineConfig } = require('cypress'); 
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({ 
   reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
       on('file:preprocessor', cucumber())
-      
+      return require('./cypress/plugins/index.js')(on,config);
     },
     specPattern: 'cypress/integration/cucumber-tests/*.feature',
 
